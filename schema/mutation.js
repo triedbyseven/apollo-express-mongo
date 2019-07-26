f2aee4e1ab4bb2917a708f15aa474f23fd57ca91
Michael Camacho <mikedev0431@gmail.com>
2019-07-26 16:56:11 -0700

    Updated type Mutation to support updateUser mutation

--- schema/mutation.js
+++ schema/mutation.js
@@ -5,6 +5,7 @@ const { gql } = require('apollo-server-express');
 const mutation = gql`
   type Mutation {
     addUser(userName: String!, email: String!, password: String!): User
+    updateUser(id: ID!, email: String!): User
     deleteUser(email: String!): User
   }
 `;
