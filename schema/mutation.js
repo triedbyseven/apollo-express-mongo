0a0e9f099b192c6d156b3b0224d7dcb523070f17
Michael Camacho <mikedev0431@gmail.com>
2019-07-27 22:01:59 -0700

    Updated addUser mutation to return AuthPayLoad type

--- schema/mutation.js
+++ schema/mutation.js
@@ -4,7 +4,7 @@ const { gql } = require('apollo-server-express');
 
 const mutation = gql`
   type Mutation {
-    addUser(userName: String!, email: String!, password: String!): User
+    addUser(userName: String!, email: String!, password: String!): AuthPayLoad!
     updateUser(id: ID!, email: String!): User
     deleteUser(email: String!): User
   }
