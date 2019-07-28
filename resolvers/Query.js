e0e71544fcf31b2803c2d14ab5c433654f08d6f9
Michael Camacho <mikedev0431@gmail.com>
2019-07-28 12:31:28 -0700

    Updated w/ comments for authenticated users

--- resolvers/Query.js
+++ resolvers/Query.js
@@ -3,7 +3,12 @@ const { User } = require('../models');
 // Query Resolvers
 
 // Return all users
-getAllUsers = async () => await User.find({}).exec();
+getAllUsers = async (_, args, { email }) => {
+  // Verify user (by email)
+  if (!email) throw new Error('Not Authenticated');
+
+  return await User.find({}).exec();
+};
 
 module.exports = {
   getAllUsers
