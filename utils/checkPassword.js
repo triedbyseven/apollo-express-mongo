7abe1c35f302560ddcc68b7c7db270b1ce01a657
Michael Camacho <mikedev0431@gmail.com>
2019-08-20 22:26:11 -0700

    Updated checkPassword function

--- utils/checkPassword.js
+++ utils/checkPassword.js
@@ -1,6 +1,7 @@
 const bcrypt = require('bcrypt');
 
 const checkPassword = async (password, hash) => {
+  // Check password against hashed password
   return await bcrypt.compare(password, hash);
 };
 
